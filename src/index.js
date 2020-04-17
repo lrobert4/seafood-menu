90326d80fd88bf52b03a50b7a33dff405f284b0a
leer <ladesigns90@gmail.com>
2020-04-17 10:46:29 -0400

    Created all my components. Created a router to route the different views.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { render } from 'react-dom';
+import Router from './components/Router';
+import './css/style.css'
 
-class StorePicker extends React.Component {
-    
-}
+render(<Router />, document.querySelector('#main'));
+
+
