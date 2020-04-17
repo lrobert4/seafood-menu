748dbeed181e9e172bfb7c227415d888b2dd2137
leer <ladesigns90@gmail.com>
2020-04-17 16:31:45 -0400

    Added the state and passed down props from App to invenotry to AddFishForm component.

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,22 @@ import Inventory from './Inventory';
 
 
 class App extends React.Component {
+
+    state = {
+        fishes: {},
+        order: {}
+    };
+
+    addFish = (fish) => {
+        // Take a copy of the existing state
+        const fishes = { ...this.state.fishes };
+        //Add our new fish to that fishes variable
+        fishes[`fish${Date.now()}`] = fish;
+        // Set the new fishes object to state
+        this.setState({ fishes });
+        
+    }
+
     render() {
 
         return (
@@ -13,8 +29,9 @@ class App extends React.Component {
                     <Header tagline="Fresh Seafood Market" />
 
                 </div>
-                <Inventory />
                 <Order />
+                <Inventory addFish={this.addFish} />
+                
 
             </div>
         )
