5b9ab5806c3a4db19d26f00ede77edc3ab7ddee1
leer <ladesigns90@gmail.com>
2020-04-18 00:28:17 -0400

    In the middle of implementing to display order state with JSX.

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,17 +1,35 @@
 import React from 'react';
+import { formatPrice } from "../helpers";
 
 class Order extends React.Component {
+    renderOrder = (key) => {
+        
+        return <li>{key}</li>
+    }
     render() {
-
+        const orderIds = Object.keys(this.props.order);
+        const total = orderIds.reduce((prevTotal, key) => {
+            const fish = this.props.fishes[key];
+            const count = this.props.order[key];
+            const isAvailable = fish && fish.status === 'available';
+            if(isAvailable) {
+                return prevTotal + (count * fish.price);
+            }
+            return prevTotal;
+        }, 0);
         return (
             
             <div className="order">
                 <h2>Order</h2>
                 <ul>
-                    <li>
-                        
-                    </li>
+
+                    {orderIds.map(this.renderOrder)}                 
                 </ul>
+                
+                <div className="total">
+                    Total:
+                    <strong>{formatPrice(total)}</strong>
+                </div>
             </div>
             
 
